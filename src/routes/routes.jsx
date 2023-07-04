9c6f048220ba06b95939def1960835caa4e2c589
Danishkhilji <danishkhilji112@gmail.com>
2023-07-05 02:04:16 +0500

    admin sidebar

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,13 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Login from '../pages/websites/login/login';
 import Signup from '../pages/websites/signup/signup';
 import Home from '../pages/websites/home';
-
+import AdminDashboard from '../pages/admin/adminDashboard';
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
+        <Route path="admin" element={<AdminDashboard />} />
+
+
     </Routes>
   )
 }
