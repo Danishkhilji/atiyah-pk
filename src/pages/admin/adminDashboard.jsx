94a0a6fed9d22db79477d3edaabdb2be144f717e
Danishkhilji <danishkhilji112@gmail.com>
2023-07-05 03:24:46 +0500

    resolve errors

--- src/pages/admin/adminDashboard.jsx
+++ src/pages/admin/adminDashboard.jsx
@@ -1,48 +1,51 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/drawer'
-import CampaignTable from '../../components/CampaignTable/CampaignTable'
-import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
-import DraftsOutlinedIcon from '@mui/icons-material/DraftsOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-const data = [
-    {
-      name: "Dashboard",
-      icon: <HomeOutlinedIcon />,
-    },
-    { name: "Inbox", icon: <InboxOutlinedIcon /> },
-    { name: "Outbox", icon: <CheckBoxOutlineBlankOutlinedIcon /> },
-    { name: "Sent mail", icon: <MailOutlineIcon /> },
-    { name: "Draft", icon: <DraftsOutlinedIcon /> },
-    { name: "Trash", icon: <ReceiptOutlinedIcon /> },
-  ];
+import React from "react";
+import Sidebar from "../../components/Sidebar/drawer";
+import CampaignTable from "../../components/CampaignTable/CampaignTable";
+import CheckBoxOutlineBlankOutlinedIcon from "@mui/icons-material/CheckBoxOutlineBlankOutlined";
+import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import AdminBarChart from "../../components/adminGraph/BarChart";
+import AdminLineChart from "../../components/adminGraph/LineChart";
 
+const data = [
+  {
+    name: "Dashboard",
+    icon: <HomeOutlinedIcon />,
+  },
+  { name: "Inbox", icon: <InboxOutlinedIcon /> },
+  { name: "Outbox", icon: <CheckBoxOutlineBlankOutlinedIcon /> },
+  { name: "Sent mail", icon: <MailOutlineIcon /> },
+  { name: "Draft", icon: <DraftsOutlinedIcon /> },
+  { name: "Trash", icon: <ReceiptOutlinedIcon /> },
+];
 
 const AdminDashboard = () => {
   return (
     <>
-    <div>
-    <Sidebar data={data}/>
+      <div>
+        <Sidebar data={data} />
         <div>
-        <h1>Analytics Overview</h1>
+          <h1>Analytics Overview</h1>
         </div>
         <div>
+        <div style={{ display: 'flex' }}>
             <div>
-                <div> Graph 1 </div>
-                <div> Graph 2 </div>
+                <AdminBarChart />
             </div>
             <div>
-            <CampaignTable/>
+                <AdminLineChart />
             </div>
-        
+            </div>
+          <div>
+            <CampaignTable />
+          </div>
         </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminDashboard
-
+export default AdminDashboard;
