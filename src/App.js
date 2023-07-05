17b77e62e850751ff96939ff0071176ab1c53a7d
Syed Ali Mashoud <mashoudntaha@gmail.com>
2023-07-05 11:01:04 +0500

    login and signup page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import './styles/responsive.css'
 import Login from './pages/websites/login/login';
+// import Signup from './pages/websites/signup/signup';
 
 function App() {
   return (
     <>
       <Login />
+      {/* <Signup /> */}
     </>
   );
 }
