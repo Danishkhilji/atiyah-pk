e9ddb0879b84bdd742240496b45be106ef3ae95f
Syed Ali Mashoud <mashoudntaha@gmail.com>
2023-07-27 03:09:12 +0500

    campaign page updated for integration

--- client/src/pages/donor/CampaignPage.jsx
+++ client/src/pages/donor/CampaignPage.jsx
@@ -34,20 +34,78 @@ export default function CampaignPage() {
                 width: '94rem'
             }}>
                 <div className="container-center">
-                    <h2 className="campaign-name">Request For Food</h2>
+                    <h2 className="campaign-name" style={{ marginLeft: '-3.9rem' }}>Request For Food</h2>
                     <div className="container-cen">
                         <div className="image">
                             <img src={Food} alt="CampaignImage" className="campaign-image" />
                             <div className="des-com">
                                 <div className="description">
                                     <h6>Decription</h6>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quibusdam, ea iste, perferendis magnam itaque repellat reiciendis ratione laboriosam nisi minus natus quidem modi possimus architecto esse consequatur numquam inventore exercitationem incidunt rem reprehenderit. Veritatis eaque sapiente, quis odit corrupti odio fugit natus totam? Quibusdam expedita incidunt in, qui blanditiis inventore, magni, deleniti molestiae iure hic minus? Eveniet et quasi placeat, quidem odio ex corrupti molestiae blanditiis cumque, fugit consequatur omnis quo nisi consequuntur nobis sapiente sed dolores eaque ipsum! Dicta hic facilis, natus a voluptatem reiciendis quo nostrum commodi expedita perferendis rerum quidem ea, veritatis, minus debitis sequi iusto esse fugit dolore amet consequatur saepe autem asperiores tempora. Unde quis nemo ea expedita ipsa, odio consequatur rem nam, harum nesciunt mollitia sequi ab itaque voluptates repudiandae. Esse quae id, vitae odit ducimus reprehenderit. Natus, vero vitae odit repudiandae cupiditate molestias! Eveniet aspernatur cumque quos, et libero, voluptates molestias saepe necessitatibus obcaecati ipsam distinctio cupiditate? Sint fugiat obcaecati voluptates neque quo, vel possimus tempore vero quos asperiores reiciendis eos ea beatae quisquam eaque in similique incidunt dolore ad atque nostrum quae soluta? Adipisci quam tempore quo ut dolores nisi exercitationem fugiat, quas nam beatae deserunt? Consectetur eius numquam non repellat!</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, minus nostrum. Velit sunt sint repellendus! Officia ea facilis dolores eaque expedita amet, assumenda voluptate numquam autem distinctio inventore. Veritatis temporibus numquam maxime officiis totam? Sit in, beatae, tempore labore enim similique et, voluptatum voluptates excepturi magni dolores numquam dolorum cumque.</p>
+                                </div>
+                                <div className="comment-section">
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
+                                    <div className="comment">
+                                        <img src={User} alt="" />
+                                        <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div id="col">
                             <div className="endorsment-sec">
-                                <h6 id="progress-data" style={{ margin: "4px auto", fontSize: "smaller" }}>
+                                <h6 id="progress-data" style={{ margin: "10px auto", fontSize: "15px" }}>
                                     Rs.{raisedAmount} Raised of Rs.{goalAmount}
                                 </h6>
                                 <ProgressBar now={progressPercentage} className="pgbar" />
@@ -57,77 +115,19 @@ export default function CampaignPage() {
                                 <Button variant="contained" style={{ background: "#117b34" }}>Share</Button>
                                 <Button variant="contained" style={{ background: "#117b34" }}>Donate Now</Button>
                             </div>
-                            <p className="a">1696 people just donated <br /> Recent Donations</p>
+                            <p className="a">1696 people just donated</p>
                             <div className="recent-donations">
                                 <div className="donation">
                                     <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor</p>
+                                    <p><b>USER123</b></p>
                                 </div>
                                 <div className="donation">
                                     <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor</p>
+                                    <p><b>USER123</b></p>
                                 </div>
                                 <div className="donation">
                                     <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor</p>
-                                </div>
-                            </div>
-                            <div className="comment-section">
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <div className="comment">
-                                    <img src={User} alt="" />
-                                    <p><b>USER123</b> <br /> Lorem ipsum dolor sit amet consectetur.</p>
+                                    <p><b>USER123</b></p>
                                 </div>
                             </div>
                         </div>
