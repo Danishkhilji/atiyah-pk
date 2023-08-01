7d70cd1be517446354930b3c554be786497f563c
Syed Ali Mashoud <mashoudntaha@gmail.com>
2023-08-02 04:04:39 +0500

    cards changed to former style and profile page ready for review

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -17,7 +17,7 @@ import ViewAll from '../pages/websites/ViewAll';
 // import RecieverProfile from '../pages/reciver/RecieverProfile';
 // import UploadCampaign from '../pages/reciver/UploadCampaign'
 import ReciverDashboard from '../pages/receiver/receiverDashboard';
-import RecieverProfile from '../pages/receiver/RecieverProfile';
+import RecieverProfile from '../pages/websites/RecieverProfile';
 import UploadCampaign from '../pages/receiver/UploadCampaign';
 import ForgetPass from '../pages/websites/login/ForgetScreens/ForgetPass';
 import OTP from '../pages/websites/login/ForgetScreens/OTP';
